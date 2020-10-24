82d55a733f9af7757e4566a4786824ff787720a7
dmitry.kan <dmitry.kan@alpha-sense.com>
2020-10-24 16:30:52 +0300

    vector search for solr 7.3.0.

--- src/main/java/com/github/saaay71/solr/VectorQuery.java
+++ src/main/java/com/github/saaay71/solr/VectorQuery.java
@@ -22,12 +22,16 @@ public class VectorQuery extends Query {
 		Weight w;
 		if(q == null){
 			w =  new ConstantScoreWeight(this, boost) {
+				public boolean isCacheable(LeafReaderContext leafReaderContext) {
+					return false;
+				}
+
 				@Override
 				public Scorer scorer(LeafReaderContext context) throws IOException {
 					return new ConstantScoreScorer(this, score(), DocIdSetIterator.all(context.reader().maxDoc()));
 				}
 			};
-		}else{
+		} else {
 			w = searcher.createWeight(q, needsScores, boost);
 		}
 		return w;
